a6d9dfdea289006e991a17d198395d375bc5fac4
Alisina <alisinasoltani82@gmail.com>
2023-10-10 22:12:11 +0330

    stat component

--- app/app/page.jsx
+++ app/app/page.jsx
@@ -1,7 +1,9 @@
+import Stat from "@/components/Stat"
+
 export default function Home() {
   return (
     <main className="">
-      ??
+      <Stat />
     </main>
   )
 }
---
+++ app/components/Stat.jsx
@@ -0,0 +1,23 @@
+export default function Stat() {
+    return (
+        <div className="stat text-black h-[230px] flex flex-row justify-center items-center absolute">
+            <div className="flex flex-row justify-center items-center gap-[6rem] relative top-[5rem]">
+                <div className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
+                rounded-2xl bg-gray-200 text-[30px] font-bold text-center">
+                    <h4>راه و ترابری</h4>
+                    <h4>3</h4>
+                </div>
+                <div className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
+                rounded-2xl bg-gray-200 text-[30px] font-bold text-center">
+                    <h4>تاسیسات و تجهیزات</h4>
+                    <h4>5</h4>
+                </div>
+                <div className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
+                rounded-2xl bg-gray-200 text-[30px] font-bold text-center">
+                    <h4>ساختمان و ابنیه</h4>
+                    <h4>5</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
