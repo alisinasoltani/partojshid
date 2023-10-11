424fac8ce6bc9e987e6911c5e75460db82ba5d06
Alisina <alisinasoltani82@gmail.com>
2023-10-11 21:38:18 +0330

    designed service container and services

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,7 +1,22 @@
 export default function Services() {
     return (
         <div className="bg-white text-black pt-[5rem] flex flex-col justify-start items-center">
-            <h2 className="services-title text-3xl font-bold py-[4rem]">خدمات ما</h2>
+            <h2 className="services-title text-3xl font-bold pt-[4rem]">خدمات ما</h2>
+            <div className="service-container grid grid-cols-4 grid-rows-1 gap-[3px] text-center place-content-center
+            my-[4rem] h-[40rem]">
+                <div className="grid col-span-1 place-content-center px-10 bg-[rgba(20,20,20,0.4)]">
+                    <h3>طراحی و اجرا</h3>
+                </div>
+                <div className="grid col-span-1 place-content-center bg-[rgba(20,20,20,0.1)]">
+                    <h3>پروژه های پیش فروش</h3>
+                </div>
+                <div className="grid col-span-1 place-content-center bg-[rgba(20,20,20,0.1)]">
+                    <h3>پروژه های فعال</h3>
+                </div>
+                <div className="grid col-span-1 place-content-center bg-[rgba(20,20,20,0.1)]">
+                    <h3>مشارکت در ساخت</h3>
+                </div>
+            </div>
         </div>
     )
 }
