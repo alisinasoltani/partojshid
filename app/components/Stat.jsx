f9d67245dbbb136bb39975248b0e1c84a54f79a9
Alisina <alisinasoltani82@gmail.com>
2023-10-11 23:46:44 +0330

    changed stat font to b roya

--- app/components/Stat.jsx
+++ app/components/Stat.jsx
@@ -3,19 +3,19 @@ export default function Stat() {
         <div className="stat text-black h-[230px] flex flex-row justify-center items-center">
             <div className="flex flex-row justify-center items-center gap-[6rem] relative top-[5rem]">
                 <div className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
-                rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl">
+                rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl roya">
                     <h4>راه و ترابری</h4>
-                    <h4>3</h4>
+                    <h4 className="text-[60px]">3</h4>
                 </div>
                 <div className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
-                rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl">
+                rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl roya">
                     <h4>تاسیسات و تجهیزات</h4>
-                    <h4>5</h4>
+                    <h4 className="text-[60px]">5</h4>
                 </div>
                 <div className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
-                rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl">
+                rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl roya">
                     <h4>ساختمان و ابنیه</h4>
-                    <h4>5</h4>
+                    <h4 className="text-[60px]">5</h4>
                 </div>
             </div>
         </div>
