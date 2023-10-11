7893ee2354a5a44fcd998e13d0b21cbaea6f8e9b
Alisina <alisinasoltani82@gmail.com>
2023-10-11 23:52:17 +0330

    changed services title font to titr

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,7 +1,7 @@
 export default function Services() {
     return (
         <div className="bg-white text-black pt-[5rem] flex flex-col justify-start items-center">
-            <h2 className="services-title text-3xl font-bold pt-[4rem]">خدمات ما</h2>
+            <h2 className="services-title text-[53px] font-bold pt-[4rem] titr">خدمات ما</h2>
             <div className="service-container grid grid-cols-4 grid-rows-1 gap-[3px] text-center place-content-center
             my-[4rem] h-[40rem]">
                 <div className="grid col-span-1 place-content-center px-10 bg-[rgba(20,20,20,0.4)]">
