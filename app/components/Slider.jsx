9d385354339e1aaacb0e38daacb242fd09c2935e
Alisina <alisinasoltani82@gmail.com>
2023-10-17 16:24:56 +0330

    changed slider components slides

--- app/components/Slider.jsx
+++ app/components/Slider.jsx
@@ -4,10 +4,10 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 // import { Navigation } from 'swiper/modules';
 import Image from 'next/image';
-import Slide1 from'@/public/images/Slide1.jpg'
-import Slide2 from'@/public/images/Slide2.jpg'
-import Slide3 from'@/public/images/Slide3.jpg'
-import Slide4 from'@/public/images/Slide4.jpg'
+import Slide1 from'@/public/images/Slide1.jpg';
+import Slide2 from'@/public/images/Slide2.jpg';
+import Slide3 from'@/public/images/Slide3.jpg';
+import Slide4 from'@/public/images/Slide4.jpg';
 
 export default function Slider() {
   return (
