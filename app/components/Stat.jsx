47e077f67c7ed9392bd1d45ef506d9eeb3ecaa4e
Alisina <alisinasoltani82@gmail.com>
2023-10-18 13:42:01 +0330

    added stat aniamtion

--- app/components/Stat.jsx
+++ app/components/Stat.jsx
@@ -1,22 +1,29 @@
+'use client'
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+
 export default function Stat() {
+    const {ref, inView, entry} = useInView({
+        threshold: 0.5,
+        triggerOnce: true,
+    });
     return (
         <div className="stat text-black h-[230px] flex flex-row justify-center items-center">
             <div className="flex flex-row justify-center items-center gap-[6rem] relative top-[5rem]">
                 <div className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
                 rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl roya">
                     <h4>ساختمان و ابنیه</h4>
-                    {/* {inView ? <CountUp className="text-[60px]" end={3} duration={4} /> : 0} */}
-                    <h4 className="text-[60px]">3</h4>
+                    {inView ? <CountUp className="text-[60px]" end={3} duration={4} /> : 0}
                 </div>
-                <div className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
+                <div ref={ref} className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
                 rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl roya">
                     <h4>تاسیسات و تجهیزات</h4>
-                    {/* {inView ? <CountUp className="text-[60px]" end={5} duration={4} /> : 0} */}
+                    {inView ? <CountUp className="text-[60px]" end={5} duration={4} /> : 0}
                     </div>
                     <div className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
                     rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl roya">
                     <h4>راه و ترابری</h4>
-                    {/* {inView ? <CountUp className="text-[60px]" end={5} duration={4} /> : 0} */}
+                    {inView ? <CountUp className="text-[60px]" end={5} duration={4} /> : 0}
                 </div>
             </div>
         </div>
