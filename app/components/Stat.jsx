ef5805300ab9448b42a3fab2d68605a20ef315b3
Alisina <alisinasoltani82@gmail.com>
2023-10-18 13:35:28 +0330

    changed slides again

--- app/components/Stat.jsx
+++ app/components/Stat.jsx
@@ -12,12 +12,10 @@ export default function Stat() {
                 rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl roya">
                     <h4>تاسیسات و تجهیزات</h4>
                     {/* {inView ? <CountUp className="text-[60px]" end={5} duration={4} /> : 0} */}
-                    <h4 className="text-[60px]">5</h4>
                     </div>
                     <div className="w-[15rem] aspect-square flex flex-col gap-4 justify-center items-center
                     rounded-2xl bg-gray-200 text-[30px] font-bold text-center shadow-xl roya">
                     <h4>راه و ترابری</h4>
-                    <h4 className="text-[60px]">5</h4>
                     {/* {inView ? <CountUp className="text-[60px]" end={5} duration={4} /> : 0} */}
                 </div>
             </div>
