60caec02c2c6b45369985f3a06e0b3ae6b78c8a6
Alisina <alisinasoltani82@gmail.com>
2023-10-20 15:49:26 +0330

    services title animation

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,6 +1,25 @@
+'user client'
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
 export default function Services() {
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.to('.service-trigger', {
+            scrollTrigger: {
+                trigger: '.service-trigger',
+                start: 'top-=200px center',
+                end: 'center-=200px center',
+                scrub: true,
+                toggleActions: 'play none none reverse',
+            },
+            '--serviceTitleBefore': '53px',
+            '--serviceTitleAfter': '53px',
+        })
+    }, []);
     return (
-        <div className="bg-white pt-[5rem] flex flex-col justify-start items-center">
+        <div className="service-trigger bg-white pt-[5rem] flex flex-col justify-start items-center">
             <h2 className="services-title text-[53px] font-bold pt-[4rem] titr text-black">خدمات ما</h2>
             <div className="service-container grid grid-cols-9 grid-rows-1 gap-[3px] text-center place-content-center
             my-[4rem] h-[40rem] text-[25px] roya">
