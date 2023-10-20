9a5a71e7d5bc422be9276b34bca3f8ab39c009ea
Alisina <alisinasoltani82@gmail.com>
2023-10-20 16:04:30 +0330

    loj animation

--- app/components/Loj.jsx
+++ app/components/Loj.jsx
@@ -1,7 +1,32 @@
+'use client'
+import { useEffect } from "react";
+import { gsap } from "gsap/gsap-core";
+import { ScrollTrigger } from "gsap/all";
 import Image from "next/image";
 import LojLogo from "@/public/images/Lodge2.png";
 
 export default function Loj() {
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.to('.loj h2', {
+            scrollTrigger: {
+                trigger: '.loj',
+                start: 'top-=100px center',
+                end: 'bottom center+=100px',
+                scrub: true,
+            },
+            x: 0,
+        })
+        gsap.to('.loj img', {
+            scrollTrigger: {
+                trigger: '.loj',
+                start: 'top-=100px center',
+                end: 'bottom center+=100px',
+                scrub: true,
+            },
+            opacity: 1,
+        })
+    }, []);
     return (
         <div className="loj flex flex-row justify-between items-end h-[35rem] px-[6rem]">
             <Image className="pb-[6rem]" src={LojLogo} width={320} alt="Loj Logo" />
