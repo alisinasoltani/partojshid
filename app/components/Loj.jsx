df2f2d42598d66d647e8406d106dc2b4f30ec633
Alisina <alisinasoltani82@gmail.com>
2023-10-28 01:00:01 +0330

    decreased loj logo size

--- app/components/Loj.jsx
+++ app/components/Loj.jsx
@@ -29,7 +29,7 @@ export default function Loj() {
     }, []);
     return (
         <div className="loj flex flex-row justify-between items-end h-[35rem] px-[6rem]">
-            <Image className="pb-[6rem]" src={LojLogo} width={320} alt="Loj Logo" />
+            <Image className="pb-[6rem]" src={LojLogo} width={300} alt="Loj Logo" />
             <h2 className="text-[68px] pb-[12rem] titr">گروه ساختمانی لژ</h2>
         </div>
     );
