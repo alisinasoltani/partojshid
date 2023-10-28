3eb377985e097113a42ecb603a58de497757749f
Alisina <alisinasoltani82@gmail.com>
2023-10-28 15:56:57 +0330

    commented next head component because of a webpack error

--- app/app/layout.jsx
+++ app/app/layout.jsx
@@ -1,5 +1,4 @@
 import './globals.css';
-// import Head from 'next/head';
 
 export const metadata = {
   title: 'Parto Jshid',
@@ -9,9 +8,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Head>
-        <link rel="shortcut icon" href="/public/logos/jeyshidLogo.png" />  
-      </Head> */}
       <body>
         {children}
       </body>
