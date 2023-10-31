be64001a18824d8edcb9045a3f0682f2bb923ddd
Alisina <alisinasoltani82@gmail.com>
2023-10-31 08:44:39 +0330

    removed another picture form the top slider

--- app/components/Slider.jsx
+++ app/components/Slider.jsx
@@ -6,8 +6,6 @@ import 'swiper/css/navigation';
 import Image from 'next/image';
 import Slide1 from'@/public/images/Slide1.jpg';
 import Slide2 from'@/public/images/Slide2.jpg';
-import Slide3 from'@/public/images/Slide3.jpg';
-import Slide4 from'@/public/images/Slide4.jpg';
 import Slide6 from'@/public/images/Slide6.jpg';
 
 export default function Slider() {
@@ -21,9 +19,6 @@ export default function Slider() {
         </SwiperSlide>
         <SwiperSlide>
           <Image src={Slide2} fill={true} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={Slide3} fill={true} />
         </SwiperSlide>
       </Swiper>
   );
