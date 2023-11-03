2da8e3064b9ac50f8105b3f75afe9b5be911c0c9
Alisina <alisinasoltani82@gmail.com>
2023-11-03 14:38:49 +0330

    pagination responsive

--- app/components/Pagination.jsx
+++ app/components/Pagination.jsx
@@ -8,15 +8,15 @@ import whatsapp from "@/public/logos/whatsapp.svg";
 
 const Pagination = () => {
   return (
-    <div className="flex flex-row-reverse koodak py-12 px-12 gap-5 text-right justify-between bg-slate-900">
+    <div className="flex md:flex-row-reverse flex-col koodak py-12 px-12 gap-10 text-right justify-between bg-slate-900">
         <div className="flex flex-col items-end">
-          <h2 className="text-3xl text-white yekanb underline underline-offset-[0.9rem] mb-10">شرکت پرتو جی شید (سهامی خاص)</h2>
+          <h2 className="md:text-3xl text-2xl text-white yekanb underline underline-offset-[0.9rem] mb-10">شرکت پرتو جی شید (سهامی خاص)</h2>
           <div className="lg:w-[25rem] flex flex-row justify-center items-start gap-2">
-            <h3 className="yekanb text-lg text-white">اصفهان، چهارباغ بالا، کوچه بیستون(12)، 
+            <h3 className="yekanb md:text-lg text-base text-white">اصفهان، چهارباغ بالا، کوچه بیستون(12)، 
               خیابان دکتر شریعتی، پلاک 124 ساختمان الماس، طبقه سوم واحد 12</h3>
             <Image src={address} width={20} alt="address logo" unoptimized />
           </div>
-          <div className="flex md:flex-row-reverse flex-col md:gap-8 gap-4 mt-10 yekanb">
+          <div className="flex md:flex-row-reverse flex-col md:gap-8 gap-4 md:mt-10 mt-5 yekanb">
             <div className="flex flex-col gap-2 text-lg text-white md:border-white md:border-l-2 pl-8">
               <div className="flex flex-row justify-end items-center gap-2">
                 <h5>09013682870</h5>
@@ -27,7 +27,7 @@ const Pagination = () => {
                 <Image src={email} width={20} alt="phone logo" unoptimized />
               </div>
             </div>
-            <div className="flex flex-col justify-center items-center gap-4 mt-[10px]">
+            <div className="flex md:flex-col flex-row justify-center items-center gap-4 mt-[10px]">
                 <button type="button" className="border-none flex flex-row justify-center gap-2 items-center py-3 px-5 font-sans" id="instagram">
                   <Image className="flex" src={instagram} width={24} />
                   <a className="flex" href="https://www.instagram.com/lodge_co/">
@@ -43,8 +43,8 @@ const Pagination = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-row items-center">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d6720.405482381656!2d51.66085300000001!3d32.627424!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzLCsDM3JzM4LjciTiA1McKwMzknMzkuMSJF!5e0!3m2!1sfa!2sde!4v1698729065136!5m2!1sfa!2sde" width="300" height="300" style={{border: '0px'}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <div className="flex flex-row items-center justify-center">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d6720.405482381656!2d51.66085300000001!3d32.627424!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzLCsDM3JzM4LjciTiA1McKwMzknMzkuMSJF!5e0!3m2!1sfa!2sde!4v1698729065136!5m2!1sfa!2sde" width="300" height="300" style={{border: '0px'}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
   );
