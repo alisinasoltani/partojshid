694584eb907bd3587e8e2615286c124e39f580ec
Alisina <alisinasoltani82@gmail.com>
2023-11-11 00:38:38 +0330

    added the projects cover for the projects page

--- app/app/projects/page.jsx
+++ app/app/projects/page.jsx
@@ -11,8 +11,13 @@ const Projects = () => {
   return (
     <>
       <Nav type={'solid'} />
-      <div className="flex flex-col bg-white mt-[6rem] text-black">
-        <div className="flex lg:flex-row flex-col items-center justify-center gap-x-[5rem] my-12">
+      <div className="flex flex-row justify-center items-center w-[100%] mt-[7rem]">
+        <div className="flex flex-row justify-center items-center projectCover w-[60rem] max-w-[60vw] h-[25rem]">
+          <h3 className="yekanb text-[50px]">پروژه های پرتو جی شید</h3>
+        </div>
+      </div>
+      <div className="flex flex-col bg-white text-black">
+        <div className="flex lg:flex-row flex-col items-center justify-center gap-x-[5rem] mt-12 mb-2">
           <div className="project flex flex-col lg:h-[30rem] mb-12 justify-start lg:items-end items-center">
             <div className="overflow-hidden mb-6 lg:w-[411px] w-[100vh] h-[285px]">
               <div className="projectImage w-[411] aspect-square">
@@ -31,7 +36,7 @@ const Projects = () => {
           </div>
         </div>
         <div className="flex lg:flex-row flex-col items-center justify-center gap-x-[5rem] bg-[#f2f5f8]">
-          <div className="project flex flex-col justify-start lg:items-end items-center my-12 lg:h-[30rem] lg:relative lg:top-[-100px]">
+          <div className="project flex flex-col justify-start lg:items-end items-center mt-2 mb-12 lg:h-[30rem] lg:relative lg:top-[-100px]">
             <div className="overflow-hidden mb-6 lg:w-[411px] w-[100vh] h-[285px]">
               <div className="projectImage w-[411] aspect-square">
                 <Image src={jeyshidkousarProject} alt="Parto Jeyshid Ejlas Project" width={411} />
@@ -49,8 +54,8 @@ const Projects = () => {
           </div>
         </div>
       </div>
-      <div className="grid grid-rows-1 place-content-center bg-white w-[80vh]">
-        <div className="projectlg grid row-span-1 lg:w-[53rem] h-[25rem] place-content-center lg:relative lg:top-[-120px]">
+      <div className="flex flex-row bg-white justify-center items-center">
+        <div className="projectXl lg:w-[60rem] flex flex-row justify-center items-center max-w-[80vw] lg:h-[25rem] lg:relative lg:top-[-120px]">
           <h3 className="yekanb text-[50px]">پروژه ساختمانی</h3>
         </div>
       </div>
