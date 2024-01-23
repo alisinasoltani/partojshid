455c964bde106e16cc1f14042322c2f7b84ad5ed
Alisina <alisinasoltani82@gmail.com>
2024-01-24 01:59:32 +0330

    changed the first slide

--- app/components/Slider.jsx
+++ app/components/Slider.jsx
@@ -8,14 +8,14 @@ import Image from "next/image";
 import Slide1 from"@/public/images/Slide1.jpg";
 import Slide2 from"@/public/images/Slide2.jpg";
 import Slide6 from"@/public/images/Slide6.jpg";
-import Slide8 from"@/public/images/Slide8.jpg";
+import Slide9 from"@/public/images/Slide9.jpg";
 
 export default function Slider() {
   return (
       <Swiper className="xl:h-[100vh] lg:h-[60vh] md:h-[40vh] h-[30vh]" modules={[Navigation, Autoplay]} navigation={true} style={{zIndex: 0}}
       loop={true} autoplay={{delay: 7000, disableOnInteraction: false}}>
         <SwiperSlide>
-          <Image src={Slide8} fill={true} />
+          <Image src={Slide9} fill={true} />
         </SwiperSlide>
         <SwiperSlide>
           <Image src={Slide6} fill={true} />
