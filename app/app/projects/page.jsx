8ba90cebbd58dfa8ed47fd45a8df579caa1a48c4
Alisina <alisinasoltani82@gmail.com>
2024-07-02 11:26:34 +0330

    feat!: added pictures for each project

--- app/app/projects/page.jsx
+++ app/app/projects/page.jsx
@@ -7,6 +7,19 @@ import jeyshidFieldProject from "@/public/images/projects/jeyshidFieldProject.jp
 import jeyshidkousarProject from "@/public/images/projects/jeyshidkousarProject.jpg";
 import jeyshidPoolProject from "@/public/images/projects/jeyshidPoolProject.jpg";
 import jeyshidFarhangProject from "@/public/images/projects/jeyshidFarhangProject.png";
+import jeyshidBaharestanProject from "@/public/images/projects/jeyshidBaharestanProject.png";
+import JeyshidConcStrucProject from "@/public/images/projects/JeyshidConcStrucProject.jpg";
+// import JeyshidCultureProject from "@/public/images/projects/JeyshidCultureProject.jpg";
+import JeyshidEngSysProject from "@/public/images/projects/JeyshidEngSysProject.jpg";
+import JeyshidGrTechCentProject from "@/public/images/projects/JeyshidGrTechCentProject.jpg";
+// import JeyshidGuardStructProject from "@/public/images/projects/JeyshidGuardStructProject.jpg";
+// import JeyshidGymProject from "@/public/images/projects/JeyshidGymProject.jpg";
+import JeyshidMatPrepProject from "@/public/images/projects/JeyshidMatPrepProject.jpg";
+import JeyshidSadraProject from "@/public/images/projects/JeyshidSadraProject.jpg";
+import JeyshidShedProject from "@/public/images/projects/JeyshidShedProject.jpg";
+import JeyshidStationProject from "@/public/images/projects/JeyshidStationProject.jpg";
+import JeyshidStorageProject from "@/public/images/projects/JeyshidStorageProject.jpg";
+
 import Lenis from "@studio-freight/lenis";
 import projectsList from "@/projects";
 import { useEffect, useState } from "react";
@@ -26,6 +39,7 @@ const Projects = () => {
     <>
       <Nav type={'solid'} lenis={lenis} />
       <div className="flex flex-col mt-[9rem] mb-[3rem] lg:mx-[5rem] mx-[2rem] gap-[3rem] items-center justify-center">
+        {/*  Ejlas */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
             <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={JeyshidEjlasProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Ejlas Saran Project" />
@@ -43,6 +57,7 @@ const Projects = () => {
             </h3>
           </div>
         </div>
+        {/* West Sports Field */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
             <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidFieldProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Sports Field Project" />
@@ -60,6 +75,7 @@ const Projects = () => {
             </h3>
           </div>
         </div>
+        {/* Kousar */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
             <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidkousarProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Kousar 2 Project" />
@@ -77,6 +93,7 @@ const Projects = () => {
             </h3>
           </div>
         </div>
+        {/* Culture Project */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
             <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidFarhangProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Cultural Center Project" />
@@ -94,6 +111,7 @@ const Projects = () => {
             </h3>
           </div>
         </div>
+        {/* Pool */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
             <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" />
@@ -111,6 +129,7 @@ const Projects = () => {
             </h3>
           </div>
         </div>
+        {/* Poolad */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
             {/* <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" /> */}
@@ -128,9 +147,10 @@ const Projects = () => {
             </h3> */}
           </div>
         </div>
+        {/* Goltaash */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
-            {/* <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" /> */}
+            <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={JeyshidMatPrepProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" />
           </div>
           <div className="flex flex-col lg:basis-8/12 basis-12/12 md:gap-8 gap-6 text-right text-black items-end justify-center px-8 py-8">
             <h2 className="font-bold lg:text-2xl text-lg">پروژه { projectsList.goltaash.name }</h2>
@@ -145,9 +165,10 @@ const Projects = () => {
             </h3> */}
           </div>
         </div>
+        {/* Engineer System */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
-            {/* <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" /> */}
+            <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={JeyshidEngSysProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" />
           </div>
           <div className="flex flex-col lg:basis-8/12 basis-12/12 md:gap-8 gap-6 text-right text-black items-end justify-center px-8 py-8">
             <h2 className="font-bold lg:text-2xl text-lg">پروژه { projectsList.engSys.name }</h2>
@@ -162,9 +183,10 @@ const Projects = () => {
             </h3> */}
           </div>
         </div>
+        {/* Bahaarestan */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
-            {/* <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" /> */}
+            <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidBaharestanProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" />
           </div>
           <div className="flex flex-col lg:basis-8/12 basis-12/12 md:gap-8 gap-6 text-right text-black items-end justify-center px-8 py-8">
             <h2 className="font-bold lg:text-2xl text-lg">پروژه { projectsList.bahaarestan.name }</h2>
@@ -179,9 +201,10 @@ const Projects = () => {
             </h3> */}
           </div>
         </div>
+        {/* Sarooj Concrete Prepration */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
-            {/* <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" /> */}
+            <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={JeyshidConcStrucProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" />
           </div>
           <div className="flex flex-col lg:basis-8/12 basis-12/12 md:gap-8 gap-6 text-right text-black items-end justify-center px-8 py-8">
             <h2 className="font-bold lg:text-2xl text-lg">پروژه { projectsList.saarooj.name }</h2>
@@ -196,26 +219,10 @@ const Projects = () => {
             </h3> */}
           </div>
         </div>
+        {/* Sadra */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
-            {/* <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" /> */}
-          </div>
-          <div className="flex flex-col lg:basis-8/12 basis-12/12 md:gap-8 gap-6 text-right text-black items-end justify-center px-8 py-8">
-            <h2 className="font-bold lg:text-2xl text-lg">پروژه { projectsList.tohid.name }</h2>
-            <h3 className="font-bold lg:text-xl text-lg">
-              تاریخ شروع: { projectsList.tohid.start }
-            </h3>
-            <h3 className="font-bold lg:text-xl text-lg">
-              تاریخ پایان: { projectsList.tohid.end }
-            </h3>
-            {/* <h3 className="font-bold lg:text-xl text-lg">
-              کارفرما : { projectsList.pool.employer }
-            </h3> */}
-          </div>
-        </div>
-        <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
-          <div className="flex flex-col lg:basis-4/12 basis-12/12">
-            {/* <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" /> */}
+            <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={JeyshidSadraProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" />
           </div>
           <div className="flex flex-col lg:basis-8/12 basis-12/12 md:gap-8 gap-6 text-right text-black items-end justify-center px-8 py-8">
             <h2 className="font-bold lg:text-2xl text-lg">پروژه { projectsList.sadraSchool.name }</h2>
@@ -230,9 +237,10 @@ const Projects = () => {
             </h3> */}
           </div>
         </div>
+        {/* Growth and Tech Center */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
-            {/* <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" /> */}
+            <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={JeyshidGrTechCentProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" />
           </div>
           <div className="flex flex-col lg:basis-8/12 basis-12/12 md:gap-8 gap-6 text-right text-black items-end justify-center px-8 py-8">
             <h2 className="font-bold lg:text-2xl text-lg">پروژه { projectsList.growthAndTech.name }</h2>
@@ -247,9 +255,10 @@ const Projects = () => {
             </h3> */}
           </div>
         </div>
+        {/* Storage */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
-            {/* <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" /> */}
+            <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={JeyshidStorageProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" />
           </div>
           <div className="flex flex-col lg:basis-8/12 basis-12/12 md:gap-8 gap-6 text-right text-black items-end justify-center px-8 py-8">
             <h2 className="font-bold lg:text-2xl text-lg">پروژه { projectsList.storage.name }</h2>
@@ -264,9 +273,10 @@ const Projects = () => {
             </h3> */}
           </div>
         </div>
+        {/* CNG Station */}
         <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
           <div className="flex flex-col lg:basis-4/12 basis-12/12">
-            {/* <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={jeyshidPoolProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" /> */}
+            <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={JeyshidStationProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" />
           </div>
           <div className="flex flex-col lg:basis-8/12 basis-12/12 md:gap-8 gap-6 text-right text-black items-end justify-center px-8 py-8">
             <h2 className="font-bold lg:text-2xl text-lg">پروژه { projectsList.cng.name }</h2>
@@ -275,6 +285,24 @@ const Projects = () => {
             </h3>
             <h3 className="font-bold lg:text-xl text-lg">
               تاریخ پایان: { projectsList.cng.end }
+            </h3>
+            {/* <h3 className="font-bold lg:text-xl text-lg">
+              کارفرما : { projectsList.pool.employer }
+            </h3> */}
+          </div>
+        </div>
+        {/* Tanker */}
+        <div className="w-full flex lg:flex-row flex-col lg:items-start justify-center rounded-2xl overflow-hidden bg-[#f4f4f4]">
+          <div className="flex flex-col lg:basis-4/12 basis-12/12">
+            <Image className="lg:min-h-[19rem] lg:min-w-[19rem]" src={JeyshidShedProject} layout={"responsive"} width={300} height={300} sizes="(max-width: 400px) 60vw, (max-width: 960px) 80vw, 95vw" alt="Parto Jeyshid Pool Project" />
+          </div>
+          <div className="flex flex-col lg:basis-8/12 basis-12/12 md:gap-8 gap-6 text-right text-black items-end justify-center px-8 py-8">
+            <h2 className="font-bold lg:text-2xl text-lg">پروژه { projectsList.tanker.name }</h2>
+            <h3 className="font-bold lg:text-xl text-lg">
+              تاریخ شروع: { projectsList.tanker.start }
+            </h3>
+            <h3 className="font-bold lg:text-xl text-lg">
+              تاریخ پایان: { projectsList.tanker.end }
             </h3>
             {/* <h3 className="font-bold lg:text-xl text-lg">
               کارفرما : { projectsList.pool.employer }
